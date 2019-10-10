9cf8d0284b3c75b6ad59385a9ec0266c9ccd9a69
Dan Whalen <queen.city.counts@gmail.com>
2019-10-10 06:08:29 -0400

    scrape address and sbl to list (single URL)

--- Get_OARS_Data.py
+++ Get_OARS_Data.py
@@ -3,10 +3,18 @@ import bs4 as bs
 source = urllib.request.urlopen('https://buffalo.oarsystem.com/assessment/results.asp?swis=140200&rptname=rpt&page=1&cnty=,%20&tbox=,%20&startdate=2000-01-01&enddate=2019-10-02&lwrsaleprice=&uprsaleprice=&lwrasmt=&uprasmt=&oname1=&lwryrbuilt=&upryrbuilt=&lwrsqft=&uprsqft=&lwrbdrms=&uprbdrms=&lwrbaths=&uprbaths=&lwrfrplcs=&uprfrplcs=&searchtype=Sales&nghdcode=&prop_class=&sch_code=&hstyle=&waterfr_type=&rswis=&overall_desire=&stname=')
 soup = bs.BeautifulSoup(source, 'lxml')
 
+raw=list()
+for tr in soup.find_all("tr"):
+    tds = tr.find_all("td")
+    raw.append(tds)
 
-body = soup.body
-b = list()
-for row in body.find_all('td'):
-	b.append(row)
-	
+data=list()
+for r in range(3,int(len(raw)-1)):
+    address = str(raw[r][1].find('span').getText())
+    sbl = str(raw[r][1])[96:108]
+    tmp = [address,sbl]
+    data.append(tmp)
+
+
+
 
