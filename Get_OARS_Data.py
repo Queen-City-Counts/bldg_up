9eccc1ac1d79bc9da425822c666b467e2194d5e7
Dan Whalen <queen.city.counts@gmail.com>
2019-10-11 17:11:52 -0400

    fixed bug in sbl_long, and bedrooms/baths/fireplaces

--- Get_OARS_Data.py
+++ Get_OARS_Data.py
@@ -11,7 +11,7 @@ for tr in soup.find_all("tr"):
 data=list()
 for r in range(3,int(len(raw)-1)):
     address = str(raw[r][1].find('span').getText())
-    sbl_long = str(raw[r][1])[96:108]
+    sbl_long = str(raw[r][1]).split(' ')[2].split('=')[-1].replace("'",'').replace('"','')
     sbl_short = str(raw[r][2].getText())
     sale_price = str(raw[r][3].getText().split(" - ")[0])
     sale_date = str(raw[r][3].getText().split(" - ")[1])
@@ -22,11 +22,15 @@ for r in range(3,int(len(raw)-1)):
     sqft = str(raw[r][8].getText())
     try:
         bedrooms = str(raw[r][9].getText().split(" / ")[0])
-        baths = str(raw[r][9].getText().split(" / ")[1])
-        fireplaces = str(raw[r][9].getText().split(" / ")[2])
     except:
         bedrooms = ""
+    try:    
+        baths = str(raw[r][9].getText().split(" / ")[1])
+    except:
         baths = ""
+    try:
+        fireplaces = str(raw[r][9].getText().split(" / ")[2])
+    except:
         fireplaces = ""
     tmp = [address, sbl_long, sbl_short, sale_price, sale_date, assessment, prop_type, lot_size, yr_blt, sqft, bedrooms, baths, fireplaces]
     data.append(tmp)
